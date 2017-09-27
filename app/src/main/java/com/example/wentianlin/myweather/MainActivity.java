eb6c79ee749bc0599a87d3739a0bff259599a547
wentianlin <7989363@qq.com>
2017-09-27 13:25:24 +0800

    init project

---
+++ app/src/main/java/com/example/wentianlin/myweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wentianlin.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wentianlin on 2017/9/27.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
